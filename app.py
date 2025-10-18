c95bef78f73c0db4a2d0bfbd6738e2b275297563
Konduru Jayanth <jayanthkumar027046@gmail.com>
2025-10-18 16:25:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("""
 <style>
 /* Main App Background */
 .stApp {
-    background: linear-gradient(135deg, #CE93D8, #AB47BC, #6A1B9A);
+    background: linear-gradient(135deg, #FFF59D, #FFEB3B, #FDD835);
     font-family: 'Arial', sans-serif;
     color: #333;
 }
@@ -254,6 +254,7 @@ st.markdown("""
 Made with ❤️ using <b>FastAPI + Streamlit</b> | Developed by <b>Konduru Jayanth</b>
 </p>
 """, unsafe_allow_html=True)
+
 
 
 
